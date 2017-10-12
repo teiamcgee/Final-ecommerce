b18a5fd24aba4cc4503050df84ae1a4d4c197780
root <root@ip-172-31-21-187.ec2.internal>
2017-10-12 21:51:32 +0000

    branch updates

--- inc/connectdb.php
+++ inc/connectdb.php
@@ -1,7 +1,7 @@
 <?php
   try{
     // makes a new connection to the database
-  $db = new PDO('mysql:dbname=Final_project;host=localhost', 'root', 'root');
+  $db = new PDO('mysql:host=localhost;dbname=tmcgee', 'r2hstudent', 'SbFaGzNgGIE8kfP');
   // check for errors
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch(Exception $e){
