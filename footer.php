a9c7da259f120059d146ca8532024acda5872108
TMcGee <teiataylor.m@gmail>
2017-10-13 09:47:15 -0400

    branch updates

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
   <div id="footer">
     <a href="https://www.linkedin.com/in/teia-taylor-mcgee-4a153354/"><img src="https://pbs.twimg.com/profile_images/614583061448036352/CBpFkPaz.png" alt="Footer"></a>
     <p>Sweetie Pie Inc</p>
+    <p>All photos and products are used for Education purposes only</p>
   </div>
 </footer>
 </body>
